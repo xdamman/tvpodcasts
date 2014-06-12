0289508fe9f791a1079ebae85ec084485fff911c
Xavier Damman <xdamman@gmail.com>
2014-06-12 12:50:20 -0700

    Updated text for rtbf feed

--- providers/rtbf.js
+++ providers/rtbf.js
@@ -17,12 +17,12 @@ module.exports = function(settings) {
   var FEED_HEADER = '<?xml version="1.0" encoding="UTF-8"?> \n\
                     <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">\n\
                     <channel> \n\
-                    \t<title>Journal 19h30 de la RTBF Video Podcast (Belgique)</title> \n\
+                    \t<title>Journal télévisé de 19h30 de la RTBF Video Podcast (Belgique)</title> \n\
                     \t<language>fr-be</language>\n\
                     \t<itunes:author>@xdamman</itunes:author>\n\
                     \t<itunes:image href="'+settings.base_url+'/img/rtbf-19h30.jpg" />\n\
-                    \t<itunes:subtitle>Video Podcast</itunes:subtitle>\n\
-                    \t<description>Retrouvez tous les jours le journal de 19h30 de la Radio Télévision Belge Francophone (RTBF) sur votre AppleTV, iPad ou iPhone.</description>\n\
+                    \t<itunes:subtitle>JT Video Podcast</itunes:subtitle>\n\
+                    \t<description>Retrouvez tous les jours le journal télévisé de 19h30 de la Radio Télévision Belge Francophone (RTBF) sur votre AppleTV, iPad ou iPhone.</description>\n\
                     \t<itunes:category text="News &amp; Politics"/>\n\
                     \t<itunes:new-feed-url>'+settings.base_url+'/feeds/rtbfpodcast.xml</itunes:new-feed-url>\n\
                     \t<link>'+settings.base_url+'/feeds/rtbfpodcast.xml</link>\n';
@@ -37,7 +37,7 @@ module.exports = function(settings) {
 
     var pages = []
       , parser = new FeedParser();
-    
+
     start_time = new Date();
 
     console.log(start_time+": Updating RSS feed");
@@ -72,7 +72,7 @@ module.exports = function(settings) {
       if(items.length == MAX_ITEMS) return;
 
       item.pubDate = (new Date(item.pubDate)).toUTCString();
-      
+
       var matches = item.guid.match(/\?id=([0-9]+)/);
       var guid = matches[1];
 
